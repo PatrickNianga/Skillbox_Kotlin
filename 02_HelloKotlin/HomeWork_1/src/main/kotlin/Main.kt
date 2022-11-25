ee151499de9895faeb77a49001124a059d8053dd
patrikniangaelenga <9062464344@mail.ru>
2022-11-25 13:42:45 +0300

    HomeWork_1 done

---
+++ 02_HelloKotlin/HomeWork_1/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    println("Привет!")
+    println("Меня зовут Патрик.")
+    //println("Я живу в Санкт-Петербурге.")
+    print("У меня нет опыта в програмировании")
+    print(" - ")
+    println("это мой первый курс!")
+    print("Тут IDE что-то хочет мне сказать, но я пока не понимаю что. ")
+    println("Можешь подсказать, что именно надо сделать в закомментированных строках?")
+    print("Спасибо!")
+/*
+    Try adding program arguments via Run/Debug configuration.
+    Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+
+ */
+}
