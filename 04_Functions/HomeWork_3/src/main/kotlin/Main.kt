3006126da63a43f48c2a8cab94249837c8dcc341
patrikniangaelenga <9062464344@mail.ru>
2022-12-08 15:01:46 +0300

    HomeWork_3 Done

--- 04_Functions/HomeWork_3/src/main/kotlin/Main.kt
+++ 04_Functions/HomeWork_3/src/main/kotlin/Main.kt
@@ -2,21 +2,25 @@ fun main() {
     val message = "F2p)v\"y233{0->c}ttelciFc"
     val decodedMessage = partOne(message) + partTwo(message)
     println(decodedMessage)
+
 }
 
 fun partOne(word: String): String {
     val firstHalf = word.take(word.length / 2)
-    val stepOne = firstHalf.map {char -> char + 1}.joinToString("")
+    val stepOne = firstHalf.map { char -> char + 1 }.joinToString("")
     val stepTwo = stepOne.replace("5", "s")
     val stepThree = stepTwo.replace("4", "u")
-    val stepFour = stepThree.map {char -> char - 3}.joinToString("")
+    val stepFour = stepThree.map { char -> char - 3 }.joinToString("")
     val stepFive = stepFour.replace("0", "o")
     return (stepFive)
 }
+
 fun partTwo(word: String): String {
     val secondHalf = word.takeLast(word.length / 2)
     val stepOnePartTwo = secondHalf.reversed()
-    val stepThreePartTwo = stepOnePartTwo.map {char -> char - 4}.joinToString("")
+    val stepThreePartTwo = stepOnePartTwo.map { char -> char - 4 }.joinToString("")
     val stepFourPartTwo = stepThreePartTwo.replace("_", " ")
     return (stepFourPartTwo)
 }
+
+
