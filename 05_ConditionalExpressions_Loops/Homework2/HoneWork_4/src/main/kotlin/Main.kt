613852f4e0ae3e2b9f9706261f400dcfd08f488c
patrikniangaelenga <9062464344@mail.ru>
2022-12-12 15:04:43 +0300

    HomeWork 4 "checking with the while loop"

--- 05_ConditionalExpressions_Loops/Homework2/HoneWork_4/src/main/kotlin/Main.kt
+++ 05_ConditionalExpressions_Loops/Homework2/HoneWork_4/src/main/kotlin/Main.kt
@@ -1,14 +1,15 @@
 fun main() {
-    fibonacciNumber()
+    fibonacciNumberForLoop()
+    fibonacciNumberWhileLoop()
 
 }
 
-fun fibonacciNumber() {
+fun fibonacciNumberForLoop() {
     println("Введите число ... !")
     val numberF: Int = readLine()?.toIntOrNull() ?: return
     if (numberF <= 0) {
         println("Вы ввели некорректное число, попробуйте снова.")
-        return fibonacciNumber()
+        return fibonacciNumberForLoop()
     }
     var numberF1 = 0
     var numberF2 = 1
@@ -19,6 +20,27 @@ fun fibonacciNumber() {
         numberF2 = numberResult
     }
     println("Число Фибоначчи с индексом $numberF равно $numberF2")
+
+}
+
+fun fibonacciNumberWhileLoop() {
+    println("Введите число ... !")
+    var numberF: Int = readLine()?.toIntOrNull() ?: return
+    if (numberF <= 0) {
+        println("Вы ввели некорректное число, попробуйте снова.")
+        return fibonacciNumberWhileLoop()
+    }
+    var numberF1 = 0
+    var numberF2 = 1
+    var i = numberF - 1
+
+    while (i>0 ) {
+        val numberResult = numberF1+numberF2
+        numberF1 = numberF2
+        numberF2 = numberResult
+        i -= 1
+    }
+    println("Номер элемента ряда Фибоначчи: $numberF \nЗначение этого элемента равно: $numberF2")
 
 }
 
