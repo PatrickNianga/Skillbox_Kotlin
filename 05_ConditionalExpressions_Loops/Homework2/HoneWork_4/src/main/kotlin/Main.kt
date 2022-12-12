8ac294a4fc6a6c39bf4798e5d5395f217999bad7
patrikniangaelenga <9062464344@mail.ru>
2022-12-12 14:30:18 +0300

    HomeWork 4 "checking with the for loop"

--- 05_ConditionalExpressions_Loops/Homework2/HoneWork_4/src/main/kotlin/Main.kt
+++ 05_ConditionalExpressions_Loops/Homework2/HoneWork_4/src/main/kotlin/Main.kt
@@ -7,9 +7,18 @@ fun fibonacciNumber() {
     println("Введите число ... !")
     val numberF: Int = readLine()?.toIntOrNull() ?: return
     if (numberF <= 0) {
-        println("Вы ввели некорректное число, поробуйте снова.")
+        println("Вы ввели некорректное число, попробуйте снова.")
         return fibonacciNumber()
     }
+    var numberF1 = 0
+    var numberF2 = 1
+
+    for (j in 2..numberF) {
+        val numberResult = numberF1+numberF2
+        numberF1 = numberF2
+        numberF2 = numberResult
+    }
+    println("Число Фибоначчи с индексом $numberF равно $numberF2")
 
 }
 
