11df7f7b963187c02d4dc648f774a7144ee60684
patrikniangaelenga <9062464344@mail.ru>
2022-12-12 14:22:41 +0300

    HomeWork 4 "checking the number > 0"

--- 05_ConditionalExpressions_Loops/Homework2/HoneWork_4/src/main/kotlin/Main.kt
+++ 05_ConditionalExpressions_Loops/Homework2/HoneWork_4/src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
 fun main() {
+    fibonacciNumber()
 
 }
+
+fun fibonacciNumber() {
+    println("Введите число ... !")
+    val numberF: Int = readLine()?.toIntOrNull() ?: return
+    if (numberF <= 0) {
+        println("Вы ввели некорректное число, поробуйте снова.")
+        return fibonacciNumber()
+    }
+
+}
+
+
+
