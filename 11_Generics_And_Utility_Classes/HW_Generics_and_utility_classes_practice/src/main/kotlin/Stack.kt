420d8e80b5e93380db2c07d5eebc4e5d4ce31d5a
patrikniangaelenga <9062464344@mail.ru>
2023-03-02 15:11:05 +0300

    the first part to check

--- 11_Generics_And_Utility_Classes/HW_Generics_and_utility_classes_practice/src/main/kotlin/Stack.kt
+++ 11_Generics_And_Utility_Classes/HW_Generics_and_utility_classes_practice/src/main/kotlin/Stack.kt
@@ -1,3 +1,17 @@
-class Stack <T> {
+class Stack<T> {
+
+    private val stackList= mutableListOf<T>()
+
+    fun push(item: T) = stackList.add(item)
+    fun pop(): T? {
+        val item = stackList.lastOrNull()
+        if (!isEmpty())
+            stackList.removeAt(stackList.size - 1)
+        return item
+    }
+    fun peek() : T? = stackList.lastOrNull()
+    fun isEmpty() =  stackList.isEmpty()
+
+    fun test() = println(stackList)
 
 }
---
+++ 11_Generics_And_Utility_Classes/HW_Generics_and_utility_classes_practice/src/main/kotlin/main.kt
@@ -0,0 +1,35 @@
+fun main(){
+
+    var stack = Stack<Int>()
+
+    stack.push(1)
+    stack.test()
+    stack.push(2)
+    stack.test()
+    stack.push(3)
+    stack.test()
+    stack.push(4)
+    stack.test()
+    println()
+    stack.test()
+    stack.pop()
+    stack.test()
+    stack.pop()
+    stack.test()
+    stack.pop()
+    stack.test()
+    println()
+    println( stack.isEmpty())
+    stack.pop()
+    stack.test()
+    println( stack.isEmpty())
+
+
+
+
+
+
+
+
+
+}
